2cd167970c56114433471976222cbd3c01cfb364
Gadi Glogowski <gadi@glogowski.ca>
2024-05-28 17:52:26 -0400

    Fixed App.test tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Welcome to SwiftCoach heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const headingElement = screen.getByText(/Welcome to SwiftCoach/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+test('renders Set Up Profile link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Set Up Profile/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders View Training Plan link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/View Training Plan/i);
   expect(linkElement).toBeInTheDocument();
 });
